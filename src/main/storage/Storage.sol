6f21cbcb2aedad2b1ef30a796bf3972d0566d0a7
shogochiai <shogo.ochiai@protonmail.com>
2024-07-05 16:36:18 +0900

    refactoring

--- src/main/storage/Storage.sol
+++ src/main/storage/Storage.sol
@@ -4,12 +4,10 @@ pragma solidity ^0.8.23;
 import "./Schema.sol";
 
 contract Storage {
-    uint256 private constant STORAGE_SLOT = uint256(keccak256("schema.globalstate"));
-
+    // cast index-erc7201 ecdysisxyz.edudao.globalstate
     function state() internal pure returns (Schema.GlobalState storage s) {
-        uint256 slot = STORAGE_SLOT;
         assembly {
-            s.slot := slot
+            s.slot := 0x6ee13987fcce5855d43f7d1fd0c7be47753eb3e4b08fa7b2f789c44ffd3a2100
         }
     }
 }
